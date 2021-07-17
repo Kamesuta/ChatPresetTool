33fcd5dbb96ad479f3f529d10e6db3bf77e0d075
Kamesuta <kamesuta@gmail.com>
2021-07-18 04:41:12 +0900

    プロジェクト ファイルを追加します。

---
+++ ChatPresetTool/BasePropertyChanged.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatPresetTool
+{
+    public abstract class BasePropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(T src, ref T dst, [CallerMemberName] string name = "")
+        {
+            if (!src.Equals(dst))
+            {
+                dst = src;
+                this.RaisePropertyChanged(name);
+            }
+        }
+    }
+}
