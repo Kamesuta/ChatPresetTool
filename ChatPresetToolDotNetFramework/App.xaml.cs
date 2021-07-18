66ef3233e5580f7aa29d85b78bf9b39b6839606e
Kamesuta <kamesuta@gmail.com>
2021-07-19 05:03:18 +0900

    多重起動

--- ChatPresetToolDotNetFramework/App.xaml.cs
+++ ChatPresetToolDotNetFramework/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Threading;
+using System.Windows;
 
 namespace ChatPresetTool
 {
@@ -7,5 +9,25 @@ namespace ChatPresetTool
     /// </summary>
     public partial class App : Application
     {
+        private readonly Mutex _mutex = new Mutex(false, Assembly.GetExecutingAssembly().GetName().Name);
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // ミューテックスの所有権を要求
+            if (!_mutex.WaitOne(0, false))
+            {
+                MessageBox.Show("Minecraftチャット便利ツールは多重起動出来ません。", "多重起動", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Shutdown();
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Close();
+            }
+        }
     }
 }
